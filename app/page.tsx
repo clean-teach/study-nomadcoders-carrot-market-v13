b3cdc41f8cdf92c7d0fcbb7e806fc21994143256
clean-teach <82921898+clean-teach@users.noreply.github.com>
2023-10-21 01:10:51 +0900

    #4.3 Test Drive part Three 및 prettier 파일 추가
    - tailwind 에는 단일 속성 클래스네임 뿐만이 아니라, 여러가지 속성이 합쳐진 기능 중심의 **helper class** 도 존재 한다.
        - 예 : `space-x-5`

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="bg-slate-400 py-20 px-20 grid gap-10">
-      <div className="bg-white p-6 rounded-3xl shadow-xl">
-        <span className="font-semibold text-3xl">Select Item</span>
-        <div className="flex justify-between my-2">
+    <div className="grid min-h-screen gap-10 bg-slate-400 px-20 py-20">
+      <div className="rounded-3xl bg-white p-6 shadow-xl">
+        <span className="text-2xl font-semibold">Select Item</span>
+        <div className="my-2 flex justify-between">
           <span className="text-gray-500">Grey Chair</span>
           <span className="font-semibold">$19</span>
         </div>
@@ -13,42 +13,76 @@ export default function Home() {
           <span className="text-gray-500">Grey Chair</span>
           <span className="font-semibold">$19</span>
         </div>
-        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+        <div className="mt-2 flex justify-between border-t-2 border-dashed pt-2">
           <span>Total</span>
           <span className="font-semibold">$10</span>
         </div>
         <div
-          className="mt-5 bg-blue-500 text-white p-3
-          text-center rounded-xl w-2/4 mx-auto
+          className="mx-auto mt-5 w-3/4 rounded-xl
+          bg-blue-500 p-3 text-center text-white
          "
         >
           Checkout
         </div>
       </div>
-      <div className="bg-white overflow-hidden rounded-3xl shadow-xl">
+      <div className="overflow-hidden rounded-3xl bg-white shadow-xl">
         <div className="bg-blue-500 p-6 pb-14">
-          <span className="text-white text-2xl">Profile</span>
+          <span className="text-2xl text-white">Profile</span>
         </div>
-        <div className="rounded-3xl p-6 bg-white relative -top-5">
-          <div className="flex relative -top-16 items-end justify-between">
+        <div className="relative -top-5 rounded-3xl bg-white p-6">
+          <div className="relative -top-16 flex items-end justify-between">
             <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-500">Orders</span>
+              <span className="text-xs text-gray-500">Orders</span>
               <span className="font-medium">340</span>
             </div>
-            <div className="h-24 w-24 bg-red-400 rounded-full" />
+            <div className="h-24 w-24 rounded-full bg-zinc-300" />
             <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-500">Spent</span>
+              <span className="text-xs text-gray-500">Spent</span>
               <span className="font-medium">$340</span>
             </div>
           </div>
-          <div className="relative  flex flex-col items-center -mt-10 -mb-5">
+          <div className="relative  -mb-5 -mt-14 flex flex-col items-center">
             <span className="text-lg font-medium">Tony Molloy</span>
             <span className="text-sm text-gray-500">미국</span>
           </div>
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="rounded-3xl bg-white p-6 shadow-xl">
+        <div className="mb-5 flex items-center justify-between">
+          <span>⬅️</span>
+          <div className="space-x-3">
+            <span>⭐️ 4.9</span>
+            <span className="rounded-md p-2 shadow-xl">💖</span>
+          </div>
+        </div>
+        <div className="mb-5 h-72 bg-zinc-400" />
+        <div className="flex flex-col">
+          <span className="text-xl font-medium">Swoon Lounge</span>
+          <span className="text-xs text-gray-500">Chair</span>
+          <div className="mb-5 mt-3 flex items-center justify-between">
+            <div>
+              <input type="radio" />
+              <input type="radio" />
+              <input type="radio" />
+            </div>
+            <div className="flex items-center space-x-5">
+              <button className=" flex aspect-square w-8 items-center justify-center rounded-lg bg-blue-200 text-xl text-gray-500">
+                -
+              </button>
+              <span>1</span>
+              <button className=" flex aspect-square w-8 items-center justify-center rounded-lg bg-blue-200 text-xl text-gray-500">
+                +
+              </button>
+            </div>
+          </div>
+          <div className="flex items-center justify-between">
+            <span className="text-2xl font-medium">$450</span>
+            <button className="rounded-lg bg-blue-500 px-8 py-2 text-center text-xs text-white">
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
