37778de3fd7b312f7ca215aaf941500235eb6be6
clean-teach <82921898+clean-teach@users.noreply.github.com>
2023-11-08 23:57:32 +0900

    #5.10 Chat Detail
    ## 목표
    
    - 채팅 상세 페이지의 UI를 만들어 본다.
    
    ## [inset](https://tailwindcss.com/docs/top-right-bottom-left)
    
    - positioned된 요소의 배치를 제어하기 위한 유틸리티입니다.
    
    ```jsx
    inset-x-0 => left: 0px; right: 0px;
    
    inset-x-2 => left: 0.5rem; /* 8px */ right: 0.5rem; /* 8px */
    
    inset-y-0 => top: 0px; bottom: 0px;
    
    inset-y-2 => top: 0.5rem; /* 8px */ bottom: 0.5rem; /* 8px */
    ```

--- app/chats/page.tsx
+++ app/chats/page.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const Chats: NextPage = () => {
   return (
     <div className="divide-y-[1px] py-10 ">
-      {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+      {[...Array(6)].map((_, i) => (
         <div
           key={i}
           className="flex cursor-pointer items-center space-x-3 px-4 py-3"
