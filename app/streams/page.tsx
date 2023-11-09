d8eb75947678a2e5aec2e239cdb0946e4070c342
clean-teach <82921898+clean-teach@users.noreply.github.com>
2023-11-10 00:48:51 +0900

    Update page.tsx

--- app/streams/page.tsx
+++ app/streams/page.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const Streams: NextPage = () => {
   return (
     <div className="space-y-4 divide-y-2 py-10">
-      {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+      {[...Array(6)].map((_, i) => (
         <div className="px-4  pt-4" key={i}>
           <div className="aspect-video w-full rounded-md bg-slate-300 shadow-sm" />
           <h3 className="mt-2 text-lg text-gray-700">Let&apos;s try potatos</h3>
