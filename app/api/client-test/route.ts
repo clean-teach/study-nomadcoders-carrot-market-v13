d50b33b4a816d1129f39253da86d955f03ed1504
clean-teach <82921898+clean-teach@users.noreply.github.com>
2023-11-16 22:21:26 +0900

    #6.7 API Routes
    ## 들어가기 전에..
    
    - client.ts 는 당연하게도 front에서 import하여 사용할 수 없음 (보안에 심각한 문제)
        - 따라서 front(브라우져)가 아닌 서버가 필요
            1. `pages` 경로에 `api` 폴더를 생성함으로써 api서버가 생성됨
            2. connection 핸들러 함수를 `export defualt`해주면 됨
    - NextJS 는 프론트엔드 뿐만 아니라, API를 만들 수도 있는 풀스텍 프레임워크이다.
        - NextJS에서 API를 만드는 방법을 알아본다.
    
    ## API Routes
    
    [Routing: API Routes](https://nextjs.org/docs/pages/building-your-application/routing/api-routes)
    
    - API route는 Next.js로 API를 빌드하기 위한 솔루션을 제공합니다.
    - `pages/api` 폴더 내의 모든 파일은 `/api/*`에 매핑되며 API endpoint로 처리됩니다.
    - server-side 전용 번들이며 client-side 번들 크기를 늘리지 않습니다.
    - `req`: http.IncomingMessage의 인스턴스와 pre-built된 일부 미들웨어
    - `res`: http.ServerResponse의 인스턴스와 일부 helper함수
    
    ## next.js 로 API Routes 만드는 방법
    
    ### 12 버전
    
    1. `pages` 폴더에 `api` 폴더를 만든다.
    2. `pages/api` 폴더에 api 파일을 만든다.
        - 예 : `client-test.tsx`
    
            ```tsx
            import { NextApiRequest, NextApiResponse } from "next";
            import client from "../../libs/client";
    
            export default async function handler(
              req: NextApiRequest,
              res: NextApiResponse
            ) {
              await client.user.create({
                data: {
                  email: "hi",
                  name: "hi",
                },
              });
              res.json({
                ok: true,
              });
            }
            ```
    
    3. 브라우저에서 접근 할 때에는 `root경로/api/api경로` 형태로 접근 한다.
    
    ### 13 버전
    
    1. `app` 폴더에 `api` 폴더를 만든다.
    2. `app/api` 폴더에 `route.ts` (혹은 `.js`) 파일을 만든다.
        1. `app/api` 폴더 아래 원하는 경로 이름으로 폴더를 만든다.
        2. 단, 파일 이름은 `route.ts` (혹은 `.js`) 여야 한다.
    3. 브라우저에서 접근 할 때에는 `root경로/api/api폴더경로` 형태로 접근 한다.
    - 참고 :
    
        [Routing: Route Handlers](https://nextjs.org/docs/app/building-your-application/routing/route-handlers)
    
        [Next.js 13버전 API 만드는 법](https://velog.io/@dokkabei97/Next.js-13버전-API-만드는-법)
    
    - [ ]  13 버전의 방법 더욱 자세히 알아볼 것

---
+++ app/api/client-test/route.ts
@@ -0,0 +1,27 @@
+import client from "@/app/_lib/client";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET() {
+  return NextResponse.json({
+  ok: true,
+  data: {
+    email: "hi",
+    name: "hi",
+  }
+  });
+  }
+
+// export default async function handler(
+//   req: NextResponse,
+//   res: NextRequest
+// ) {
+//   await client.user.create({
+//     data: {
+//       email: "hi",
+//       name: "hi",
+//     },
+//   });
+//   res.json({
+//     ok: true,
+//   });
+// }
