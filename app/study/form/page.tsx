b47c54fecdcc4e656102c5e68c144f15f47769c5
clean-teach <82921898+clean-teach@users.noreply.github.com>
2023-11-21 00:33:42 +0900

    #7.5 Errors
    ## 목표
    
    - 커스텀(사용자 정의) 유효성 검사 속성을 알아본다.
        - API를 사용하여 이미 가입된 사용자를 확인하거나
        - 필요한 조건을 직접 만들어서 사용 할 수 있다.
    - 화면에 에러 메세지를 표시해 본다.
    - 유효성 검사를 실행 할 시점, 즉 form 이벤트를 선택 할 수 있다.
        - 예 : submit, change, blur .. 등
    
    ## validate
    
    - 유효성을 검사할 인수로 콜백 함수를 전달하거나 콜백 함수의 개체를 전달하여 모든 유효성을 검사할 수 있습니다
    
    ### 사용법
    
    ```tsx
    ...register("", {
    	...
    	validate : value => value === '1'
    })
    ```
    
    - 조건 자체를 반환 할 수도 있고
    - 조건에 따라서 문자열을 반환하면, 에러 메세지를 반환 한다.
    
    ## `[formState: {errors}](https://react-hook-form.com/docs/useform/formstate)`
    
    - 함수 밖에서도 에러 메세지를 출력 할 수 있다.
    
        → 리액트 상태(state) 처럼 화면에 출력 할 수 있다.
    
        → 에러 상태를 이용해서 실시간으로 스타일을 적용 할 수도 있다.
    
    ### 사용법
    
    ```tsx
    const {register, formState: {errors}, ...} = useForm({
    	mode: **"" // <- 모드 선택** onChange | onBlur | onSubmit | onTouched | all
    });
    ```
    
    ## [mode](https://react-hook-form.com/docs/useform#mode)
    
    - 이 옵션을 사용하면 사용자가 form을 제출하기 전에 유효성 검사를 할 수 있습니다
        - 기본 값 : `onSubmit`
        - `onChange | onBlur | onSubmit | onTouched | all = 'onSubmit’`
    
    ### 사용법
    
    ```tsx
    const {formState: {errors}} = useForm();
    
    ...
    <p>{errors.email.message}</p>
    ```
    
    - `onTouched` : 유효성 검사는 첫 번째 blur 이벤트에서 트리거됩니다. 그 후에는 모든 change 이벤트에서 트리거됩니다.
    - `all` : blur 및 change 이벤트에서 유효성 검사가 트리거됩니다.

--- app/study/form/page.tsx
+++ app/study/form/page.tsx
@@ -8,7 +8,13 @@ interface LoginForm {
 }
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm<LoginForm>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    mode: "onChange",
+  });
   const onValid = (data: LoginForm) => {
     console.log("im valid bby");
     console.log(data);
@@ -16,6 +22,8 @@ export default function Forms() {
   const onInvalid = (errors: FieldErrors) => {
     console.log(errors);
   };
+
+  console.log(errors);
 
   return (
     <form onSubmit={handleSubmit(onValid, onInvalid)}>
@@ -30,7 +38,14 @@ export default function Forms() {
         type="text"
         placeholder="Username"
       />
-      <input {...register("email", { required: "Email is required" })} type="email" placeholder="Email" />
+      <input {...register("email", {
+          required: "Email is required",
+          validate: {
+            notGmail: (value) =>
+              !value.includes("@gmail.com") || "Gmail is not allowed",
+          },
+        })} type="email" placeholder="Email" className={`${Boolean(errors.email) ? 'border-red-500' : ''}`} />
+      {errors.email?.message}
       <input
         {...register("password", { required: "Password is required" })}
         type="password"
